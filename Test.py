0d605b2d54068d710815834990a68b144a98ccdc
Aleksandr <alesavigo@gmail.com>
2020-11-25 21:55:50 +0700

    Solved!

--- Test.py
+++ Test.py
@@ -23,19 +23,31 @@ def area(X_list, Y_list):
             break
         Minus_Part += float(X_list[i + 1] * Y_list[i])
         i += 1
-    S = float(0.5 * abs(Plus_Part - Minus_Part))
-    print(S)
+    S = round(float(0.5 * abs(Plus_Part - Minus_Part)), 1)
+    return S
 
 
-def taking_coordinates_lon(latitude, longitude):
-    # result = float(latitude*(2*pi_c * RE) / 360)
-    result = RE * cos(radians(latitude*2*pi_c / 360))*cos(radians(longitude * 2 * pi_c / 360))
+def taking_coordinates_lon(latitude):
+    result = 2 * pi_c * RE / 360 * (latitude - average_latitude)
     return result
 
 
 def taking_coordinates_lat(longitude):
-    # result = float(longitude * koef * (2*pi_c * RE) / 360)
+    koef = cos(radians(average_latitude))
+    result = float((longitude - average_longitude)
+                   * koef * (2*pi_c * RE) / 360)
     return result
+
+
+def fill_nulls(number):
+    number = str(number)
+    if len(number) == 3:
+        number = "000" + number
+    if len(number) == 4:
+        number = "00" + number
+    if len(number) == 5:
+        number = "0" + number
+    return number
 
 
 # Constant
@@ -48,28 +60,25 @@ Y_list = []
 
 for i in input().split(' '):
     list_final.append((list(map(float, i.split(":")))))
-print(list_final)
 
 for i in range(len(list_final)):
     egrees_list.append(conversion_to_egrees(list_final[i]))
-print(egrees_list)
+
 count = sum = 0
 for i in range(1, len(egrees_list), 2):
     count += 1
     sum += egrees_list[i]
-
 average_longitude = float(sum / count)
-koef = cos(radians(average_longitude))
 
-
-x = taking_coordinates_lon(egrees_list[0], egrees_list[1])
-print(x)
-
+count = sum = 0
+for i in range(0, len(egrees_list), 2):
+    count += 1
+    sum += egrees_list[i]
+average_latitude = float(sum / count)
 
 for i in range(0, len(egrees_list), 2):
     X_list.append(taking_coordinates_lat(egrees_list[i]))
 for i in range(1, len(egrees_list), 2):
     Y_list.append(taking_coordinates_lon(egrees_list[i]))
 
-print(X_list, Y_list)
-area(X_list, Y_list)
+print(fill_nulls(area(X_list, Y_list)))
